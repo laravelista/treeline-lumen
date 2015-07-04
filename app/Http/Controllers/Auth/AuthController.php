d382e9889245d92d6dc4c32e6fb20172eaaaf260
Mario Basic <mario.basic@outlook.com>
2015-07-04 14:41:36 +0200

    Added a configure directive for jwt config.

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -58,7 +58,7 @@ class AuthController extends Controller {
     /**
      * Get the needed authorization credentials from the request.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return array
      */
     protected function getCredentials(Request $request)
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,6 +21,8 @@ $app = new Laravel\Lumen\Application(
 
 $app->withFacades();
 
+$app->configure('jwt');
+
 class_alias('Tymon\JWTAuth\Facades\JWTAuth', 'JWTAuth');
 /** This gives you finer control over the payloads you create if you require it.
  *  Source: https://github.com/tymondesigns/jwt-auth/wiki/Installation
